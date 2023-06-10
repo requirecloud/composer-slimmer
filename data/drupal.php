6c5680c6d96c44a4dfd3895192f2ba51450de2c7
Marko Korhonen <marko.korhonen@druid.fi>
2023-06-10 09:14:52 +0300

    Add mode Drupal core profiles

--- data/drupal.php
+++ data/drupal.php
@@ -5,7 +5,17 @@ return [
         'drupal/core' => [
             'folders' => [
                 'profiles/demo_umami',
-                'profiles/testing',
+                'profiles/nightwatch_testing',
+                'profiles/testing_config_import',
+                'profiles/testing_config_overrides',
+                'profiles/testing_install_profile_all_dependencies',
+                'profiles/testing_install_profile_dependencies',
+                'profiles/testing_missing_dependencies',
+                'profiles/testing_multilingual',
+                'profiles/testing_multilingual_with_english',
+                'profiles/testing_requirements',
+                'profiles/testing_site_config',
+                'profiles/testing_themes_blocks',
             ],
         ],
     ],

59f4441fc7f7bd5d42aa4eca42300607916d7354
Marko Korhonen <marko.korhonen@druid.fi>
2023-06-11 23:04:30 +0300

    Load drupal rules once

--- src/Cleaner.php
+++ src/Cleaner.php
@@ -11,6 +11,8 @@ class Cleaner
 
     private string $packagePath;
 
+    private array $extras = [];
+
     public function __construct(IOInterface $io)
     {
         $this->io = $io;
@@ -22,9 +24,12 @@ class Cleaner
         $extra = [];
 
         if (str_contains($package->getPrettyName(), 'drupal/')) {
-            $this->io->write('<info>Loading Drupal package rules...</info>');
-            $drupalRules = require __DIR__ . '/../data/drupal.php' ?? [];
-            $extra = $drupalRules[$package->getPrettyName()] ?? [];
+            if (empty($this->extras['drupal'])) {
+                $this->io->write('<info>Loading Drupal package rules...</info>');
+                $this->extras['drupal'] = require __DIR__ . '/../data/drupal.php' ?? [];
+            }
+
+            $extra = $this->extras['drupal'][$package->getPrettyName()] ?? [];
         }
 
         $recursiveCleaner = new RecursiveCleaner($this->io);
